1d62898af7c3b5b17fe5bb9bec2e2b361ad0ab5f
danieltonad <soladan2000@gmail.com>
2024-03-22 21:51:33 -0900

    done?

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from os import getenv
+import uvicorn
 
 app = FastAPI(title='FastApi on vercel')
 
 @app.get('/')
 async def root():
     return "Welcome to FastAPI on Vercel"
+
+
+if __name__ == "__main__":
+    port = int(getenv("PORT", 8000))
+    uvicorn.run("main:app", host="127.0.0.1", port=port ,reload=True)
